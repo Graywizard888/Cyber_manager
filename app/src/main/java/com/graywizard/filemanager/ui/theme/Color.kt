3dbd7701266a2f2f1cd69dcb3dce24df23da7a61
Aditya <adityajasud8@gmail.com>
2026-04-22 16:03:32 +0530

    Create Color.kt

---
+++ app/src/main/java/com/graywizard/filemanager/ui/theme/Color.kt
@@ -0,0 +1,56 @@
+package com.graywizard.filemanager.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Light Theme Colors
+val PrimaryLight = Color(0xFF6366F1)
+val OnPrimaryLight = Color(0xFFFFFFFF)
+val PrimaryContainerLight = Color(0xFFE0E7FF)
+val OnPrimaryContainerLight = Color(0xFF1E1B3E)
+
+val SecondaryLight = Color(0xFF8B5CF6)
+val OnSecondaryLight = Color(0xFFFFFFFF)
+val SecondaryContainerLight = Color(0xFFF3E5F5)
+val OnSecondaryContainerLight = Color(0xFF2E1B47)
+
+val TertiaryLight = Color(0xFF06B6D4)
+val OnTertiaryLight = Color(0xFFFFFFFF)
+val TertiaryContainerLight = Color(0xFFE0F2FE)
+val OnTertiaryContainerLight = Color(0xFF0C4A6E)
+
+val BackgroundLight = Color(0xFFFCFCFF)
+val OnBackgroundLight = Color(0xFF1A1C1E)
+val SurfaceLight = Color(0xFFFCFCFF)
+val OnSurfaceLight = Color(0xFF1A1C1E)
+
+val ErrorLight = Color(0xFFEF4444)
+val OnErrorLight = Color(0xFFFFFFFF)
+
+// Dark Theme Colors
+val PrimaryDark = Color(0xFF818CF8)
+val OnPrimaryDark = Color(0xFF000000)
+val PrimaryContainerDark = Color(0xFF3730A3)
+val OnPrimaryContainerDark = Color(0xFFE0E7FF)
+
+val SecondaryDark = Color(0xFFA78BFA)
+val OnSecondaryDark = Color(0xFF000000)
+val SecondaryContainerDark = Color(0xFF6D28D9)
+val OnSecondaryContainerDark = Color(0xFFF3E5F5)
+
+val TertiaryDark = Color(0xFF22D3EE)
+val OnTertiaryDark = Color(0xFF000000)
+val TertiaryContainerDark = Color(0xFF0E7490)
+val OnTertiaryContainerDark = Color(0xFFE0F2FE)
+
+val BackgroundDark = Color(0xFF0F172A)
+val OnBackgroundDark = Color(0xFFE2E8F0)
+val SurfaceDark = Color(0xFF1E293B)
+val OnSurfaceDark = Color(0xFFE2E8F0)
+
+val ErrorDark = Color(0xFFF87171)
+val OnErrorDark = Color(0xFF000000)
+
+// Additional Colors
+val CyberBlue = Color(0xFF00F0FF)
+val CyberPurple = Color(0xFFBF00FF)
+val CyberPink = Color(0xFFFF006E)
