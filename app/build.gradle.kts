d7cca78063b5ffcccd1cd71893468bbaff9fddf9
Aditya <adityajasud8@gmail.com>
2026-04-23 18:24:19 +0530

    Update CMake version from 4.3.2 to 3.22.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,7 @@ android {
     externalNativeBuild {
         cmake {
             path = file("src/main/cpp/CMakeLists.txt")
-            version = "4.3.2"
+            version = "3.22.1"
         }
     }
 
